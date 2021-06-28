2ac04c375c32e2d315012c9a550cb253ee2f4a65
Kazuki Okamoto <kiki.com.start@gmail.com>
2021-06-28 23:03:56 +0900

    バンザイポーズの追加

--- myblock.js
+++ myblock.js
@@ -25,7 +25,26 @@ Blockly.defineBlocksWithJsonArray(
     "colour": 230,
     "tooltip": "",
     "helpUrl": ""
-  }]
+  },
+  {
+    "type": "banzai",
+    "message0": "%1",
+    "args0": [
+      {
+        "type": "field_image",
+        "src": "https://2.bp.blogspot.com/-C_ZLBrSz3B0/Uvy5z1KOCJI/AAAAAAAAdqE/Jtqur1Qwd6o/s800/banzai_boy.png",
+        "width": 100,
+        "height": 100,
+        "alt": "*",
+        "flipRtl": false
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },]
 )
 
 Blockly.JavaScript['hand_goo'] = function(block) {
@@ -45,5 +64,11 @@ Blockly.JavaScript['hand_pa'] = function(block) {
 Blockly.JavaScript['hand_choki'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
   var code = 'チョキ,';
+  return code;
+};
+
+Blockly.JavaScript['banzai'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
   return code;
 };
